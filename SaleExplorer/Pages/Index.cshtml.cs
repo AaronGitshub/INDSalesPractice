1a1396363e2a5ad3694bb675c178ae4f2e6c9993
AaronGitsHub <aaronwood5@mac.com>
2019-04-26 16:32:08 -0400

    first commit

---
+++ SaleExplorer/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace SaleExplorer.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public string MapboxAccessToken { get; }
+
+        public IndexModel(IConfiguration configuration)
+        {
+            MapboxAccessToken = configuration["Mapbox:AccessToken"];
+        }
+
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
