c071988a762c21e798a8265bb4f6f2da812dbd3f
AaronGitsHub <aaronwood5@mac.com>
2019-06-19 10:45:37 -0400

    Map loads to Indy view and shows 5 sales

--- SaleExplorer/Pages/Index.cshtml.cs
+++ SaleExplorer/Pages/Index.cshtml.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -10,16 +16,75 @@ namespace SaleExplorer.Pages
 {
     public class IndexModel : PageModel
     {
-
+        private readonly IHostingEnvironment _hostingEnvironment;
         public string MapboxAccessToken { get; }
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
+
             MapboxAccessToken = configuration["Mapbox:AccessToken"];
         }
 
+        public IActionResult OnGetSales()
+        {
+            var configuration = new Configuration
+            {
+                BadDataFound = context => {}
+            };
+
+            using (var sr = new StreamReader(Path.Combine(_hostingEnvironment.WebRootPath, "2018MarionCountyIND_xlsWLatLong.dat")))
+            using (var reader = new CsvReader(sr, configuration))
+            {
+                FeatureCollection featureCollection = new FeatureCollection();
+
+                while (reader.Read())
+                {
+                    string saleDisclosureId = reader.GetField<string>(0);
+                    string parcelNumber = reader.GetField<string>(1);
+                    string numberOfParcels = reader.GetField<string>(2);
+                    string saleStreet = reader.GetField<string>(3);
+                    string saleCity = reader.GetField<string>(4);
+                    string saleZip = reader.GetField<string>(5);
+                    string saleConveyanceDate = reader.GetField<string>(6);
+                    string salesPrice = reader.GetField<string>(7);
+                    string buyer1 = reader.GetField<string>(8);
+                    string seller1 = reader.GetField<string>(15);
+                    string propertyClassCode = reader.GetField<string>(27);
+                    string saleParcelAcreage = reader.GetField<string>(30);
+                    string saleDate = reader.GetField<string>(31);
+                    string dateRecieved = reader.GetField<string>(32);
+                    string transferDate = reader.GetField<string>(33);
+                    double lat = reader.GetField<double>(56);
+                    double longitude = reader.GetField<double>(57);
+
+                    featureCollection.Features.Add(new Feature(
+                        new Point(new Position(lat, longitude)),
+                        new Dictionary<string, object>
+                        {
+                            {"saleDisclosureId", saleDisclosureId},
+                            {"parcelNumber", parcelNumber},
+                            {"numberOfParcels", numberOfParcels},
+                            {"saleStreet", saleStreet},
+                            {"saleCity", saleCity},
+                            {"saleZip", saleZip},
+                            {"saleConveyanceDate", saleConveyanceDate},
+                            {"salesPrice", salesPrice},
+                            {"buyer1", buyer1},
+                            {"seller1", seller1},
+                            {"propertyClassCode", propertyClassCode},
+                            {"saleParcelAcreage", saleParcelAcreage},
+                            {"saleDate", saleDate},
+                            {"dateRecieved", dateRecieved},
+                            {"transferDate", transferDate},
+                        }));
+                }
+            return new JsonResult(featureCollection);
+            }
 
 
+        }
+        // TODO: 
         public void OnGet()
         {
 
--- SaleExplorer/obj/Debug/netcoreapp2.1/Razor/Pages/Shared/_CookieConsentPartial.g.cshtml.cs
+++ SaleExplorer/obj/Debug/netcoreapp2.1/Razor/Pages/Shared/_CookieConsentPartial.g.cshtml.cs
@@ -91,7 +91,7 @@ using Microsoft.AspNetCore.Http.Features;
                     ");
             EndContext();
             BeginContext(1234, 69, false);
-            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "c80804f042624385b66a03dff89ebdb6", async() => {
+            __tagHelperExecutionContext = __tagHelperScopeManager.Begin("a", global::Microsoft.AspNetCore.Razor.TagHelpers.TagMode.StartTagAndEndTag, "144503f60f2c476793aa12a9d5fa9a69", async() => {
                 BeginContext(1289, 10, true);
                 WriteLiteral("Learn More");
                 EndContext();
